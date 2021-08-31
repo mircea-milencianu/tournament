9284e305b3caba4918c787d02f4124c360ce7ebb
Mircea Milencianu <mircea.milencianu@gmail.com>
2021-08-31 21:57:00 +0300

    turneu cu deviatie

--- research_proj/debug_tour.py
+++ research_proj/debug_tour.py
@@ -3,5 +3,5 @@ import axelrod as axl
 players =  [axl.Cooperator(), axl.Defector(),
             axl.TitForTat()]
 
-tournament = axl.Tournament(players, turns=4, repetitions=2)
+tournament = axl.Tournament(players, turns=20, deviation=11, repetitions=2)
 results = tournament.play(filename="basic_tournament.csv")
