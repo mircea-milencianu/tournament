5db1588e2e53b20952a76575bbe297ed8cb3795a
Mircea Milencianu <mircea.milencianu@gmail.com>
2021-09-02 21:48:06 +0300

    02.09.2021

--- research_proj/debug_tour.py
+++ research_proj/debug_tour.py
@@ -3,5 +3,5 @@ import axelrod as axl
 players =  [axl.Cooperator(), axl.Defector(),
             axl.TitForTat()]
 
-tournament = axl.Tournament(players, turns=20, deviation=11, repetitions=2)
-results = tournament.play(filename="basic_tournament.csv")
+tournament = axl.Tournament(players, turns=100, deviation=5, repetitions=2)
+matrix_test = tournament.play(filename="basic_tournament.csv")
