bca1bf05fdc8dd0b559cb73dc4fc386062606683
Mircea Milencianu <mircea.milencianu@gmail.com>
2021-09-06 22:11:35 +0300

    matrice finala

--- research_proj/debug_tour.py
+++ research_proj/debug_tour.py
@@ -3,5 +3,5 @@ import axelrod as axl
 players =  [axl.Cooperator(), axl.Defector(),
             axl.TitForTat()]
 
-tournament = axl.Tournament(players, turns=100, deviation=5, repetitions=2)
+tournament = axl.Tournament(players, turns=100, deviation=5, repetitions=5)
 matrix_test = tournament.play(filename="basic_tournament.csv")
