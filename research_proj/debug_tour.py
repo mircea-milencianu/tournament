4d45da2b60d9b9ee19b70faa9bd77f6599db43db
Mircea Milencianu <mircea.milencianu@gmail.com>
2021-09-07 22:06:18 +0300

    07.09.2021

--- research_proj/debug_tour.py
+++ research_proj/debug_tour.py
@@ -6,6 +6,6 @@ players =  [axl.Cooperator(), axl.Defector(),
             axl.TitForTat()]
 
 mc_tournament = axl.McTournament(players, turns=100, deviation=5, repetitions=5)
-winner_set = mc_tournament.play(filename="basic_tournament.csv")
+winner_set = mc_tournament.play(filename="mc_tournament.csv")
 
 winner_matrix = winner_set.build_winner_pd()
