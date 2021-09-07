f1432ec518b2c79e6c162cf6988b81f151fe447f
Mircea Milencianu <mircea.milencianu@gmail.com>
2021-09-07 22:00:37 +0300

    07.09.2021

--- research_proj/debug_tour.py
+++ research_proj/debug_tour.py
@@ -1,7 +1,11 @@
 import axelrod as axl
+import pprint as p
+
 
 players =  [axl.Cooperator(), axl.Defector(),
             axl.TitForTat()]
 
-tournament = axl.Tournament(players, turns=100, deviation=5, repetitions=5)
-matrix_test = tournament.play(filename="basic_tournament.csv")
+mc_tournament = axl.McTournament(players, turns=100, deviation=5, repetitions=5)
+winner_set = mc_tournament.play(filename="basic_tournament.csv")
+
+winner_matrix = winner_set.build_winner_pd()
