512f93e58098f63f34d2985a6f0c1e44442cb9ab
Mircea Milencianu <mircea.milencianu@gmail.com>
2021-09-09 19:38:32 +0300

    def main

--- research_proj/full_tour.py
+++ research_proj/full_tour.py
@@ -1,15 +1,19 @@
 import axelrod as axl
 import pprint as p
 
+players = [s() for s in axl.axelrod_first_strategies]
 
-players =  [axl.Cooperator(), axl.Defector(),
-            axl.TitForTat()]
 
-tournament = axl.Tournament(players, turns=100, deviation=5, repetitions=1)
-result_set, result_matrix = tournament.play(filename="new_tournament.csv")
+# players =  [axl.Cooperator(), axl.Defector(),
+#             axl.TitForTat()]
+def main():
+    tournament = axl.Tournament(players, turns=200, deviation=10, repetitions=1)
+    result_set, result_matrix = tournament.play(filename="new_tournament.csv", processes=2)
 
 # print(result_matrix)
 # print(result_set)
-winner_matrix = result_matrix.build_winner_pd()
-
+    winner_matrix = result_matrix.build_winner_pd()
 # print(winner_matrix)
+
+if __name__ == "__main__":
+    main()
