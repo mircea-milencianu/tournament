eb6bb431a6b97db5f4f65688d76566805a3f51fe
Mircea Milencianu <mircea.milencianu@gmail.com>
2021-09-30 23:22:56 +0300

    30.09.2021

--- research_proj/full_tour.py
+++ research_proj/full_tour.py
@@ -4,27 +4,29 @@ import pprint as p
 from timeit import default_timer as timer
 
 
-#players = [s() for s in axl.axelrod_first_strategies]
-
-players_first = [s() for s in axl.axelrod_first_strategies]
-players_second = [s() for s in axl.axelrod_second_strategies]
-
-players = players_first + players_second
+player_set = {
+    
+    "dev_tour" : [axl.Cooperator, axl.Defector, axl.TitForTat],
+    "first_tour" : [s() for s in axl.axelrod_first_strategies],
+    "second_tour" : [s() for s in axl.axelrod_second_strategies],
+    "all" : [s() for s in axl.all_strategies],
+}
 
 def main():
     ### INIT ###
+    players = player_set["dev_tour"]
     start = timer()
     tournament = axl.Tournament(players, turns=200, deviation=20, repetitions=200)
     end = timer()
     print("time spent for init: {}".format(end - start))
     ### PLAY ###
     start = timer()
-    result_set, result_matrix = tournament.play(filename="new_tournament.csv", processes=0)
+    result_set = tournament.play(filename="new_tournament.csv", processes=0)
     end = timer()
     print("time spent for playing the matches: {}".format(end - start))
     ### EXPORT ###
     start = timer()
-    winner_matrix = result_matrix.build_winner_pd()
+    winner_matrix = axl.ResultMatrix(players).create(filename="new_tournament.csv")
     end = timer()
     print("time spent for playing the matches: {}".format(end - start))
 
