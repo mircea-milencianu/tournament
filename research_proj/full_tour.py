b95e778e74d722e27a3e3c5ad41fd0c6fb3748df
Mircea Milencianu <mircea.milencianu@gmail.com>
2021-10-05 10:40:54 +0300

    final matrix

--- research_proj/full_tour.py
+++ research_proj/full_tour.py
@@ -3,6 +3,9 @@ import pprint as p
 
 from timeit import default_timer as timer
 
+from axelrod import tournament
+from axelrod import result_set
+
 
 player_set = {
     
@@ -14,20 +17,24 @@ player_set = {
 
 def main():
     ### INIT ###
-    players = player_set["first_tour"] # + player_set["second_tour"]
+    players = player_set["first_tour"] + player_set["second_tour"]
     start = timer()
-    tournament = axl.Tournament(players, turns=200, deviation=20, repetitions=1)
+    tournament_default = axl.Tournament(players, turns=200, repetitions=1000)
+    tournament_mc = axl.Tournament(players, turns=200, deviation=40, repetitions=1000)
     end = timer()
     print("time spent for init: {}".format(end - start))
     ### PLAY ###
     start = timer()
-    result_set = tournament.play(filename="new_tournament.csv",processes=0) #
+    result_set_default = tournament_default.play(filename="tournament_default.csv",processes=4) #
+    result_set_mc = tournament_mc.play(filename="tournament_mc.csv",processes=4) #
     end = timer()
     print("time spent for playing the matches: {}".format(end - start))
     ### EXPORT ###
     start = timer()
-    matrix = axl.ResultMatrix(filename="new_tournament.csv", players=players, repetitions=200)
-    winner_matrix = matrix.create()
+    matrix_default = axl.ResultMatrix(filename="tournament_default.csv", players=players, repetitions=1000, tour_type="default_devNone")
+    matrix_mc = axl.ResultMatrix(filename="tournament_mc.csv", players=players, repetitions=1000, tour_type="montecarlo_dev40")
+    winner_matrix = matrix_mc.create()
+    winner_matrix = matrix_default.create()
     end = timer()
     print("time spent for building the matrix: {}".format(end - start))
 
