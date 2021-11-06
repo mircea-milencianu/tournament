88e06e2fc7fa47055c09d2a2df70051d89c89fc8
Mircea Milencianu <mircea.milencianu@gmail.com>
2021-11-06 14:17:04 +0200

    uniform distribution & utility vectors matrix

--- research_proj/full_tour.py
+++ research_proj/full_tour.py
@@ -8,35 +8,49 @@ from axelrod import result_set
 
 
 player_set = {
-    
-    "dev_tour" : [axl.Cooperator(), axl.Defector(), axl.TitForTat()],
-    "first_tour" : [s() for s in axl.axelrod_first_strategies],
-    "second_tour" : [s() for s in axl.axelrod_second_strategies],
-    "all" : [s() for s in axl.all_strategies],
+    "dev_tour": [axl.Cooperator(), axl.Defector(), axl.TitForTat()],
+    "first_tour": [s() for s in axl.axelrod_first_strategies],
+    "second_tour": [s() for s in axl.axelrod_second_strategies],
+    "all": [s() for s in axl.all_strategies],
 }
+
 
 def main():
     ### INIT ###
-    players = player_set["first_tour"] + player_set["second_tour"]
+    #players = player_set["first_tour"] + player_set["second_tour"]
+    players = player_set["dev_tour"]
     start = timer()
-    tournament_default = axl.Tournament(players, turns=200, repetitions=1000)
-    tournament_mc = axl.Tournament(players, turns=200, deviation=40, repetitions=1000)
+    tournament_default = axl.Tournament(players, turns=100, repetitions=5)
+    tournament_mc = axl.Tournament(players, turns=100, uniform=True, deviation=10, repetitions=1)
     end = timer()
     print("time spent for init: {}".format(end - start))
     ### PLAY ###
     start = timer()
-    result_set_default = tournament_default.play(filename="tournament_default.csv",processes=4) #
-    result_set_mc = tournament_mc.play(filename="tournament_mc.csv",processes=4) #
+    result_set_default = tournament_default.play(
+        filename="tournament_default.csv", processes=4
+    )  #
+    result_set_mc = tournament_mc.play(filename="tournament_mc.csv", processes=4)  #
     end = timer()
     print("time spent for playing the matches: {}".format(end - start))
     ### EXPORT ###
     start = timer()
-    matrix_default = axl.ResultMatrix(filename="tournament_default.csv", players=players, repetitions=1000, tour_type="default_devNone")
-    matrix_mc = axl.ResultMatrix(filename="tournament_mc.csv", players=players, repetitions=1000, tour_type="montecarlo_dev40")
+    matrix_default = axl.ResultMatrix(
+        filename="tournament_default.csv",
+        players=players,
+        repetitions=1000,
+        tour_type="default_devNone",
+    )
+    matrix_mc = axl.ResultMatrix(
+        filename="tournament_mc.csv",
+        players=players,
+        repetitions=1000,
+        tour_type="montecarlo_dev40",
+    )
     winner_matrix = matrix_mc.create()
     winner_matrix = matrix_default.create()
     end = timer()
     print("time spent for building the matrix: {}".format(end - start))
+
 
 if __name__ == "__main__":
     main()
