b476c000b7e310bcc1939e4022a8b229c1d705bd
Mircea Milencianu <mircea.milencianu@gmail.com>
2022-07-21 19:22:24 +0300

    simple run

--- research_proj/full_tour.py
+++ research_proj/full_tour.py
@@ -7,7 +7,7 @@ from axelrod import tournament
 from axelrod import result_set
 
 TURNS = 200
-REPETITIONS = 100
+REPETITIONS = 1
 RUN_TYPE = "dev"
 
 player_set = {
@@ -37,29 +37,44 @@ def step_run(players, deviation):
 
 def main():
     ### INIT ###
-    # players = player_set["first_tour"] + player_set["second_tour"]
+    # players = player_set["dev_tour"] + player_set["second_tour"]
     players = player_set["dev_tour"]
 
-    ### run basic tour with no dev
-    tournament_default = axl.Tournament(players, turns=TURNS, repetitions=REPETITIONS)
-    result_set_default = tournament_default.play(
-        filename="tournament_default.csv", processes=4
-    )  
-    matrix_default = axl.ResultMatrix(
-        filename="tournament_default.csv",
+    tournament_mc = axl.Tournament(
+        players, turns=TURNS, uniform=True, deviation=deviation, repetitions=REPETITIONS
+    )
+    result_set_mc = tournament_mc.play(filename="tournament_mc.csv", processes=4)  #
+    end = timer()
+    matrix_mc = axl.ResultMatrix(
+        filename="tournament_mc.csv",
         players=players,
         repetitions=REPETITIONS,
-        deviation=None,
-        run_type="default"
+        run_type="montecarlo",
+        tour_type="dev",
     )
-    winner_matrix = matrix_default.create()
-    ### run basic tour with no dev ###############################
+    winner_matrix = matrix_mc.create()
 
-    deviation = 100
-    step = 5
-    while deviation >= 80:
-        step_run(players, deviation)
-        deviation = deviation - step
+
+    ### run basic tour with no dev
+    # tournament_default = axl.Tournament(players, turns=TURNS, repetitions=REPETITIONS)
+    # result_set_default = tournament_default.play(
+    #     filename="tournament_default.csv", processes=4
+    # )  
+    # matrix_default = axl.ResultMatrix(
+    #     filename="tournament_default.csv",
+    #     players=players,
+    #     repetitions=REPETITIONS,
+    #     deviation=None,
+    #     run_type="default"
+    # )
+    # winner_matrix = matrix_default.create()
+    # ### run basic tour with no dev ###############################
+
+    # deviation = 20
+    # step = 2
+    # while deviation >= 1:
+    #     step_run(players, deviation)
+    #     deviation = deviation - step
     
 
 if __name__ == "__main__":
